793a08ceb9254276680e25d550e38585d3e9d366
Sanya Kumari <1701sanya@gmail.com>
2020-11-08 03:05:40 +0530

    Final project!

---
+++ date.js
@@ -0,0 +1,15 @@
+//jshint
+
+module.exports = getDate;
+
+function getDate(){
+const today = new Date();
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+
+return today.toLocaleDateString("hi-IN", options);
+}
